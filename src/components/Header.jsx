788e14e8902a9f305a63d472b7cca506d30dcaf8
CesarMC <cesar_mejia_99@hotmail.com>
2025-01-14 16:43:37 -0500

    agregado estilos de la navegacion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import { Navigation } from "./Navigation";
 
 export const Header = () => {
   return (
-    <header className="bg-red-700 h-screen w-screen">
-      <div className="fixed w-screen top-0 left-0">
+    <header className="bg-red-700 h-screen">
+      <div className="fixed w-screen top-0 left-0 grid place-items-center bg-slate-200/80">
         <Navigation></Navigation>
       </div>
     </header>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,15 +3,15 @@ import { links } from "./links";
 
 export const Navigation = () => {
   return (
-    <div>
+    <div className="flex justify-between items-center w-[80%] p-2">
       <div>
         <img className="w-[80px]" src="./imgs/LOGO3.jpeg" alt="logo-page" />
       </div>
       <nav>
-        <ul>
+        <ul className="flex gap-10">
           {links.map((link) => (
             <li key={link.id}>
-              <HashLink to={link.link}>{link.name}</HashLink>
+              <HashLink className="text-zinc-900 text-xl font-semibold transition-all duration-300 backdrop-blur-200 backdrop-saturate-200 hover:text-red-500" to={link.link}>{link.name}</HashLink>
             </li>
           ))}
         </ul>
